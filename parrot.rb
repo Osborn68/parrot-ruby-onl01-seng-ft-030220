c60ea0d55ea5aa574343ef99dabf6ea29e91f94a
Christopher Osborn <z68ozz@gmail.com>
2020-03-04 04:17:12 +0000

    Automatically backed up by Learn

--- parrot.rb
+++ parrot.rb
@@ -1,8 +1,8 @@
 # Create method `parrot` that outputs a given phrase and
 # returns the phrase
-def parrot(phrase="Squawk!",name="Pretty bird!")
+def parrot(phrase="Pretty bird!")
   puts "#{phrase}"
-  puts "#{name}"
+
   "Squawk!"
 
 end
